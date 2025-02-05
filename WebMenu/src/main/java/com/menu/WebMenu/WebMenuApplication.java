0f637afa2026d5d36b5c25b6129b2e81efc3045f
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-02-05 18:18:41 -0300

    first commit, loading project.

---
+++ WebMenu/src/main/java/com/menu/WebMenu/WebMenuApplication.java
@@ -0,0 +1,13 @@
+package com.menu.WebMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMenuApplication.class, args);
+	}
+
+}
---
+++ WebMenu/src/test/java/com/menu/WebMenu/WebMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menu.WebMenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
